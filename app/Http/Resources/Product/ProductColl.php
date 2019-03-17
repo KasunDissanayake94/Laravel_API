d10c74dbf5db6e8c3b0df24f965ddf9ef70fe093
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2019-03-17 16:47:56 +0530

    Product Collection and redirection for Single product created

---
+++ app/Http/Resources/Product/ProductColl.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductColl extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'discount' => $this->discount,
+            'href' => [
+                'href' => route('products.show',$this->id)
+            ]
+
+        ];
+    }
+}
